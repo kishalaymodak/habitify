cb01a363bcdfdf76f2c9f38c833e5e576df3c6a1
kishalaymodak <kishalaymodak@gmail.com>
2025-04-04 20:39:42 +0530

    get email password route closed

--- web/src/app/api/profile/route.ts
+++ web/src/app/api/profile/route.ts
@@ -3,48 +3,48 @@ import bcrypt from "bcryptjs";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const session = await getServerSession();
-  try {
-    if (!session?.user?.email) {
-      return NextResponse.json(
-        {
-          message: "you are not logedIn",
-        },
-        { status: 403 }
-      );
-    }
+// export async function GET() {
+//   const session = await getServerSession();
+//   try {
+//     if (!session?.user?.email) {
+//       return NextResponse.json(
+//         {
+//           message: "you are not logedIn",
+//         },
+//         { status: 403 }
+//       );
+//     }
 
-    const user = await prisma.user.findUnique({
-      where: {
-        userEmail: session?.user?.email || "",
-      },
-    });
-    if (!user || user == null) {
-      return NextResponse.json(
-        {
-          message: "user not found. please LogIn or SignUp",
-        },
-        { status: 403 }
-      );
-    }
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         userEmail: session?.user?.email || "",
+//       },
+//     });
+//     if (!user || user == null) {
+//       return NextResponse.json(
+//         {
+//           message: "user not found. please LogIn or SignUp",
+//         },
+//         { status: 403 }
+//       );
+//     }
 
-    return NextResponse.json({
-      message: "Authenticated",
-      email: user.userEmail,
-      password: user.password,
-    });
-  } catch (error) {
-    console.log("profile data fetching" + error);
+//     return NextResponse.json({
+//       message: "Authenticated",
+//       email: user.userEmail,
+//       password: user.password,
+//     });
+//   } catch (error) {
+//     console.log("profile data fetching" + error);
 
-    return NextResponse.json(
-      {
-        message: "error while fetching user data",
-      },
-      { status: 404 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       {
+//         message: "error while fetching user data",
+//       },
+//       { status: 404 }
+//     );
+//   }
+// }
 
 export async function POST(req: NextRequest) {
   const {
